f315a8df55dd52bb277130aa9b199c49abcd3fd6
Lukasz Modzelewski <lukaszmod@o2.pl>
2021-01-14 20:50:03 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import { Home, Results } from './containers';
+import { Home, Results, PageNotFound } from './containers';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
 import './App.scss';
 
 function App() {
+  const search = 'nature';
+
   return (
-    <div className="App">
-      <Home />
-      <Results />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/results">
+            <Results search={search} />
+          </Route>
+          <Route
+            path="/page-not-found"
+            render={() => <PageNotFound path="/" />}
+          />
+          <Redirect to="page-not-found" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
